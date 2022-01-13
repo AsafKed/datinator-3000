1653a8e19a66ad7242e61b2592322df591aa53e8
AsafKed <asafkedem97@gmail.com>
2022-01-13 15:57:17 +0100

    initial commit

---
+++ services/DatesService.js
@@ -0,0 +1,40 @@
+const DATES = [
+    {
+        id: 100,
+        name: 'ReactProX Headset',
+        price: 350,
+        image: require('../assets/products/headset-100.jpg'),
+        description: 'A headset combines a headphone with microphone. Headsets are made with either a single-earpiece (mono) or a double-earpiece (mono to both ears or stereo).'
+    },
+    {
+        id: 101,
+        name: 'FastLane Toy Car',
+        price: 600,
+        image: require('../assets/products/car-101.jpg'),
+        description: 'A model car, or toy car, is a miniature representation of an automobile. Other miniature motor vehicles, such as trucks, buses, or even ATVs, etc. are often included in this general category.'
+    },
+    {
+        id: 102,
+        name: 'SweetHome Cupcake',
+        price: 2,
+        image: require('../assets/products/cake-102.jpg'),
+        description: 'A cupcake (also British English: fairy cake; Hiberno-English: bun; Australian English: fairy cake or patty cake[1]) is a small cake designed to serve one person.'
+    }
+];
+
+
+export function getDates() {
+    // DATES = fetch('http://asafkedem.com/api/v1')
+    //         .then(response => response.json())
+    //         .then(data => console.log(data))
+    //         .catch(error => console.log(error));
+    return DATES;
+}
+export function getDate(id) {
+    return DATES.find((product) => (id == id));
+}
+
+// add ; delimiter
+// var results = Papa.parse("../../Dates.csv");
+// console.log(results.meta.delimiter);
+// console.log(results)
